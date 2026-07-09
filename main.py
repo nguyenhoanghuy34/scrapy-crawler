44b52e375b7d43369a7457b40ad1dd100195e070
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-07-10 01:05:24 +0700

    main.mainpy

---
+++ main.py
@@ -0,0 +1,21 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+
+    start_urls = [
+        "https://books.toscrape.com/"
+    ]
+
+    def parse(self, response):
+        titles = response.css("h3 a::attr(title)").getall()
+
+        for title in titles:
+            print(title)
+
+
+process = CrawlerProcess()
+process.crawl(BooksSpider)
+process.start()
